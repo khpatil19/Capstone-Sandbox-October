7759a42de5028a6130e596be9c83e74de0a99a12
Kaustubh Patil <68739277+khpatil19@users.noreply.github.com>
2024-10-24 10:49:59 -0500

    commit2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
+import pandas as pd
+from joblib import load
 import streamlit as st
 
-st.title("🎈 My new app")
+# Load the model from the file
+rf_pipeline = load('C:/Users/khpat/Desktop/Desktop Files/LabelMaster/random_forest_regressor.joblib')
+
+
+st.title("Capstone model demo")
 st.write(
     "Testing changes khpatil"
 )
